91a552ff7dd626a42166ed04931c6d00a53d7677
sim <sim@cheolhwan.com>
2025-04-30 12:11:21 +0900

    Update dependencies

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { WalletWidgetProvider } from "@initia/react-wallet-widget"
+import { TESTNET, WalletWidgetProvider } from "@initia/react-wallet-widget"
 import App from "./app/App"
 
 const queryClient = new QueryClient()
@@ -9,20 +9,9 @@ const queryClient = new QueryClient()
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <WalletWidgetProvider
-        registryUrl="https://registry.testnet.initia.xyz"
-        apiUrl="https://api.testnet.initia.xyz"
-        dexApiUrl="https://dex-api.testnet.initia.xyz"
-        explorerUrl="https://scan.testnet.initia.xyz"
-        swaplistUrl="https://list.testnet.initia.xyz/pairs.json"
-        modules={{
-          usernames: "0x42cd8467b1c86e59bf319e5664a09b6b5840bb3fac64f5ce690b5041c530565a",
-          dex_utils: "0x42cd8467b1c86e59bf319e5664a09b6b5840bb3fac64f5ce690b5041c530565a",
-          swap_transfer: "0x42cd8467b1c86e59bf319e5664a09b6b5840bb3fac64f5ce690b5041c530565a",
-        }}
-      >
+      <WalletWidgetProvider {...TESTNET}>
         <App />
       </WalletWidgetProvider>
     </QueryClientProvider>
-  </StrictMode>,
+  </StrictMode>
 )
