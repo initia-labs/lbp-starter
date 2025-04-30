9ba7b8320eda49a09b0574e6e3593bc00415c348
sim <sim@cheolhwan.com>
2025-04-30 12:10:24 +0900

    Switch order of the base/quote coin for more realistic example

--- frontend/src/data/constants.ts
+++ frontend/src/data/constants.ts
@@ -1,6 +1,6 @@
 export const restUrl = "https://rest.testnet.initia.xyz"
 
-export const pair = "0x7c4b0843e2544ec4e09f459a24edee3172f84f7a07a1fd78c49cd1fa4bd14d87"
+export const pair = "0xf9bf6fe0e4e060135704917866bd35889ac7898c45bf0416b354acdc2205fc6e"
 
 export interface PairCoin {
   denom: string
@@ -10,18 +10,18 @@ export interface PairCoin {
 }
 
 export const coinA: PairCoin = {
-  denom: "uinit",
-  metadata: "0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9",
-  symbol: "INIT",
-  decimals: 6,
-}
-
-export const coinB: PairCoin = {
   denom: "utia",
   metadata: "0xacceb3b245392afe08346b794cf5c4ff85e7e9a8c82fcaf5112ae9d64ba57ccb",
   symbol: "TIA",
   decimals: 6,
 }
 
-export const baseCoin = coinA
-export const quoteCoin = coinB
+export const coinB: PairCoin = {
+  denom: "uinit",
+  metadata: "0x8e4733bdabcf7d4afc3d14f0dd46c9bf52fb0fce9e4b996c939e195b8bc891d9",
+  symbol: "INIT",
+  decimals: 6,
+}
+
+export const baseCoin = coinB
+export const quoteCoin = coinA
